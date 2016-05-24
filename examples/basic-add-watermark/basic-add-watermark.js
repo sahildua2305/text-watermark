2cd6c36debc0ea27e7658215f44847d3c65cbb67
Sahil Dua <sahildua2305@gmail.com>
2016-05-24 13:31:29 +0530

    Minor changes in config of package

--- examples/basic-add-watermark/basic-add-watermark.js
+++ examples/basic-add-watermark/basic-add-watermark.js
@@ -3,7 +3,7 @@
 * @Author: sahildua2305
 * @Date:   2016-05-24 06:59:44
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-24 13:26:47
+* @Last Modified time: 2016-05-24 13:29:53
 */
 
 'use strict';
