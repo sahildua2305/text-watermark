76a5f5df897583d9c8d0efcea506ba8bcb6ad7b2
Sahil Dua <sahildua2305@gmail.com>
2016-05-24 07:19:27 +0530

    Example basic watermark edit

--- examples/basic-add-watermark/basic-add-watermark.js
+++ examples/basic-add-watermark/basic-add-watermark.js
@@ -3,7 +3,7 @@
 * @Author: sahildua2305
 * @Date:   2016-05-24 06:59:44
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-24 07:09:22
+* @Last Modified time: 2016-05-24 07:14:05
 */
 
 'use strict';
@@ -12,7 +12,7 @@ var fs = require('fs');
 var im = require('imagemagick');
 var path = require('path');
 var ratify = require('node-ratify');
-var watermark = require('../../');
+var watermark = require('../../text-watermark.js');
 
 var options = {
     'text' : 'Sahil Dua'
