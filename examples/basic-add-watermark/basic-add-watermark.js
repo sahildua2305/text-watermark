09963375a2b63a9a75117578248a26a225228a1c
Sahil Dua <sahildua2305@gmail.com>
2016-05-24 07:12:54 +0530

    Added example

---
+++ examples/basic-add-watermark/basic-add-watermark.js
@@ -0,0 +1,21 @@
+/*
+* @Package: text-watermark
+* @Author: sahildua2305
+* @Date:   2016-05-24 06:59:44
+* @Last Modified by:   Sahil Dua
+* @Last Modified time: 2016-05-24 07:09:22
+*/
+
+'use strict';
+
+var fs = require('fs');
+var im = require('imagemagick');
+var path = require('path');
+var ratify = require('node-ratify');
+var watermark = require('../../');
+
+var options = {
+    'text' : 'Sahil Dua'
+};
+
+watermark.addWatermark('/home/sahildua2305/Downloads/sahildua.jpg', options);
--- text-watermark.js
+++ text-watermark.js
@@ -1,16 +1,17 @@
 /*
+* @Package: text-watermark
 * @Author: sahildua2305
 * @Date:   2016-05-24 06:44:49
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-24 06:56:20
+* @Last Modified time: 2016-05-24 07:03:22
 */
 
 'use strict';
 
-var fs = require('fs'),
-    im = require('imagemagick'),
-    path = require('path'),
-    ratify = require('node-ratify');
+var fs = require('fs');
+var im = require('imagemagick');
+var path = require('path');
+var ratify = require('node-ratify');
 
 var defaultOptions = {
     "text" : "Sample Text Watermark",
