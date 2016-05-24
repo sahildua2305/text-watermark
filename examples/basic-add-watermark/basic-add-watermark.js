22e5e3653cbb99e7ea67e85f61db2ff1ffb88adc
Sahil Dua <sahildua2305@gmail.com>
2016-05-24 13:28:10 +0530

    Resolving require issues in examples

--- examples/basic-add-watermark/basic-add-watermark.js
+++ examples/basic-add-watermark/basic-add-watermark.js
@@ -3,7 +3,7 @@
 * @Author: sahildua2305
 * @Date:   2016-05-24 06:59:44
 * @Last Modified by:   Sahil Dua
-* @Last Modified time: 2016-05-24 07:14:05
+* @Last Modified time: 2016-05-24 13:26:47
 */
 
 'use strict';
@@ -12,10 +12,15 @@ var fs = require('fs');
 var im = require('imagemagick');
 var path = require('path');
 var ratify = require('node-ratify');
-var watermark = require('../../text-watermark.js');
+var watermark = require('text-watermark');
 
 var options = {
     'text' : 'Sahil Dua'
 };
 
-watermark.addWatermark('/home/sahildua2305/Downloads/sahildua.jpg', options);
+watermark.addWatermark('/home/sahildua2305/Downloads/sahildua.jpg', options, function(err){
+	if(err)
+		return console.log(err);
+
+	return console.log("Successful");
+});
